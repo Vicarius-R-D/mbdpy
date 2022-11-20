ab2a579a245e49fc524a53b616d2bf00560fe6d8
Mattia-Vicari <mattia.vic99@gmail.com>
2022-11-20 19:59:45 +0100

    display equal blocks with labels #9

--- mbdpy/frontend.py
+++ mbdpy/frontend.py
@@ -1,76 +1,39 @@
-from PyQt5.QtCore import QSize, Qt
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QToolBar, QAction, QStatusBar, QCheckBox
-from PyQt5.QtGui import QPalette, QColor, QIcon
+from PyQt5.QtWidgets import QGraphicsScene, QGraphicsView, QGraphicsRectItem, QApplication, QGraphicsTextItem
+from PyQt5.QtGui import QBrush, QPen
+from PyQt5.QtCore import Qt
+from .model import Model
 
-class Color(QWidget):
-
-    def __init__(self, color):
-        super(Color, self).__init__()
-        self.setAutoFillBackground(True)
-
-        palette = self.palette()
-        palette.setColor(QPalette.Window, QColor(color))
-        self.setPalette(palette)
-
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super(MainWindow, self).__init__()
-
-        self.setWindowTitle("MBDpy")
-
-        layout1 = QHBoxLayout()
-
-        lateral_menu = QWidget()
-        lateral_menu.setMinimumWidth(520)
-        lateral_menu.setMaximumWidth(520)
-        lateral_menu.setMinimumHeight(1080)
-        lateral_menu.setAutoFillBackground(True)
-        palette = lateral_menu.palette()
-        palette.setColor(QPalette.Window, QColor('grey'))
-        lateral_menu.setPalette(palette)
-
-        model_space = QWidget()
-        model_space.setMinimumWidth(1400)
-        model_space.setMinimumHeight(1080)
-        model_space.setAutoFillBackground(True)
-        palette = model_space.palette()
-        palette.setColor(QPalette.Window, QColor('white'))
-        model_space.setPalette(palette)
-
-        layout1.addWidget(lateral_menu)
-        layout1.addWidget(model_space)
-
-        layout1.setSpacing(0)
-
-        widget = QWidget()
-        widget.setLayout(layout1)
-        self.setCentralWidget(widget)
-
-        button_action = QAction(QIcon("bug.png"), "&Your button", self)
-        button_action.setStatusTip("This is your button")
-        button_action.triggered.connect(self.onMyToolBarButtonClick)
-        button_action.setCheckable(True)
-
-        button_action2 = QAction(QIcon("bug.png"), "Your &button2", self)
-        button_action2.setStatusTip("This is your button2")
-        button_action2.triggered.connect(self.onMyToolBarButtonClick)
-        button_action2.setCheckable(True)
-
-        menu = self.menuBar()
-
-        file_menu = menu.addMenu("&File")
-        file_menu.addAction(button_action)
-        file_menu.addSeparator()
-        file_menu.addAction(button_action2)
-    
-    def onMyToolBarButtonClick(self, s):
-        print("click", s)
 
 class App:
-    def __init__(self) -> None:
+    def __init__(self, model: Model) -> None:
         self.app = QApplication([])
-        self.window = MainWindow()
-        self.window.show()
+        # Defining a scene rect of 1280x720, with it's origin at 0, 0.
+        # If we don't set this on creation, we can set it later with .setSceneRect
+        scene = QGraphicsScene(0, 0, 1280, 720)
+
+        for block in model.blocks:
+            # Draw a rectangle item, setting the dimensions.
+            rect = QGraphicsRectItem(0, 0, block.dimension[0], block.dimension[1])
+
+            # Set the origin (position) of the rectangle in the scene.
+            rect.setPos(block.coord[0], block.coord[1])
+
+            # Define the brush (fill).
+            brush = QBrush(Qt.white)
+            rect.setBrush(brush)
+
+            # Define the pen (line)
+            pen = QPen(Qt.black)
+            pen.setWidth(2)
+            rect.setPen(pen)
+
+            label = QGraphicsTextItem(parent=rect)
+            label.setTextWidth(block.dimension[0])
+            label.setHtml('<center>' + block.label + '</center>')
+
+            scene.addItem(rect)
+        self.view = QGraphicsView(scene)
+        self.view.show()
 
     def execute(self):
         self.app.exec()
