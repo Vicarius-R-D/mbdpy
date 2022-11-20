5ae34821d0393f4bcccb1e124a1f96a3185b2a0c
Mattia-Vicari <mattia.vic99@gmail.com>
2022-11-20 11:31:37 +0100

    add Terminator block

--- mbdpy/blocks.py
+++ mbdpy/blocks.py
@@ -1,5 +1,7 @@
 from .model import Model
 
+import numpy as np
+
 class Block:
 
     def __init__(self):
@@ -9,18 +11,40 @@ class Block:
         self.output_to = input_id
 
 
+class Constant(Block):
+
+    def __init__(self, value: float):
+        self.input_id = []
+        self.input_buffer = [None]
+        self.value = value
+        self.output_to = None
+
+    def evaluate(self, inputs: list) -> float:
+
+        return self.value
+
+
+class Terminator(Block):
+
+    def __init__(self, model: Model):
+        self.input_id = [model.create_port_id()]
+        self.input_buffer = [None]
+        self.output_to = None
+
+    def evaluate(self, input: list[float]) -> None:
+        pass
+
 class Add(Block):
 
     def __init__(self, n_input, model: Model):
         self.input_id = []
+        self.input_buffer = []
         self.output_to = None
 
         for _ in range(n_input):
             self.input_id.append(model.create_port_id())
+            self.input_buffer.append(None)
 
+    def evaluate(self, inputs: list[float]) -> float:
 
-class Constant(Block):
-
-    def __init__(self, value):
-        self.value = value
-        self.output_to = None
+        return np.sum(np.array(inputs))
